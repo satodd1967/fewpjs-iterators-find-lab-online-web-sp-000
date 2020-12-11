fa98c26cbd4db0570cf5762cc69ff391bfd805b0
Seth Todd <seth.todd@outlook.com>
2020-12-10 20:49:38 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+let superbowlWin = function(array) {
+  let win = array.find(o => o.result === "W");
+  if (!!win == false) {return undefined
+  } else {
+    return win.year
+  }
+  
+}
